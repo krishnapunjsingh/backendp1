01f55a6946168593f0eefeb800df427ac982ee0f
Krishna punj singh <krishnapunjsingh@gmail.com>
2025-02-13 00:15:41 +0530

    all setup configured

---
+++ src/routes/user.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { resisterUser} from "../controllers/user.controller.js"
+
+const router = Router()
+
+router.route("/register").post(resisterUser)
+
+export default router
